320a6924a1f5e03d4a569994006442535f151ea5
Samuel Thompson <thomps9012@gmail.com>
2020-09-25 10:23:31 -0400

    added in base level component did mount for quotes page

--- src/App.js
+++ src/App.js
@@ -8,21 +8,19 @@ import Quote from './quotes/quote';
 
 function App() {
   return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Put some random positiveness in your day
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Put some random positiveness in your day
         </p>
 
         <Button size="lg" variant="primary" onClick={Quote}>
-
-  
-            Yes Please
+          Yes Please
           </Button>
 
-        </header>
-      </div>
+      </header>
+    </div>
   );
 }
 
--- src/quotes/quote.js
+++ src/quotes/quote.js
@@ -1,56 +1,27 @@
 import axios from 'axios';
-import React, { useState, useEffect, setContent, response } from 'react';
+import React from 'react';
 import './quote.css';
 export default Quote;
 
 
-function Quote() {
-    
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
+export default class Quote extends React.Component {
+    state = {
+        quote: []
+    }
 
-    // Note: the empty deps array [] means
-    // this useEffect will run once
-    // similar to componentDidMount()    
-    useEffect(() => {
-        axios({
-            "method": "GET",
-            "url": "https://quotes15.p.rapidapi.com/quotes/random/",
-            "headers": {
-                "content-type": "application/octet-stream",
-                "x-rapidapi-host": "quotes15.p.rapidapi.com",
-                "x-rapidapi-key": "a5431a964dmsh3fe2f2b97c70f4ep1c2083jsnb0757f5b9779",
-                "useQueryString": true
-            }, "params": {
-                "language_code": "en"
-            }
-        })
-            .then(response => response.json())
-            .then(
-                (response) => {
-                    console.log(response)
-                    setIsLoaded(true);
-                    setContent(response.content);
-                },
-                (error) => {
-                    setIsLoaded(true);
-                    setError(error);
-                    console.log(error)
-                }
-            )
-    }, [])
-    if (error) {
-        return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-        return <div>Loading...</div>;
-    } else {
-        return (
-            <div>
-                <h1>
-                    {response.content}
-                </h1>
-            </div>
-
-        );
+    componentDidMount() {
+        axios.get(`https://quotes15.p.rapidapi.com/quotes/random/`)
+            .then(res => {
+                const quote = res.data;
+                this.setState({ quote });
+            })
+    }
+    render() {
+        return(
+            <ul>
+            { this.state.quote.map(quote => <li>{quote.content}</li>) }
+            { this.state.quote.map(quote => <li>{quote.orginator.name}</li>) }
+            </ul>
+        )
     }
 }
