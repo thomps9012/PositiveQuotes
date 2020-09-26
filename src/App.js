08ee772fe264fb2af1efc60ec092c994ff6ec760
Samuel Thompson <thomps9012@gmail.com>
2020-09-26 09:27:20 -0400

    api call working correctly

--- src/App.js
+++ src/App.js
@@ -4,16 +4,35 @@ import './App.css';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Quote from './quotes/quote';
+import api from './quotes/api';
 
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      quote: null,
+      name: null
+    };
+  }
+  render(){
+    
+    const { quote, name } = this.state;
+
+    return(
+
+      <div className="App">
+
       <header className="App-header">
+
         <img src={logo} className="App-logo" alt="logo" />
+
         <p>
           Put some random positiveness in your day
         </p>
+
+        <Quote quote={quote} name={name} />
 
         <Button size="lg" variant="primary" onClick={Quote}>
           Yes Please
@@ -21,7 +40,31 @@ function App() {
 
       </header>
     </div>
-  );
+    );
+  }
+
+  async componentDidMount(){
+    
+    let quoteData = await api.get('/', {
+      params: {
+        results: 1,
+        inc: 'quote, name'
+      }
+    });
+
+    quoteData = quoteData.data.results[0];
+
+    const quote = quoteData.content;
+    const name = quoteData.orginator.name;
+
+    this.setState({
+      ...this.state, ...{
+        quote,
+        name
+      }
+    });
+  }
 }
 
 export default App;
+  
--- src/quotes/quote.js
+++ src/quotes/quote.js
@@ -1,27 +1,33 @@
-import axios from 'axios';
+
+import PropTypes from 'prop-types';
 import React from 'react';
 import './quote.css';
-export default Quote;
+import Card from 'react-bootstrap/Card';
 
-
-export default class Quote extends React.Component {
-    state = {
-        quote: []
-    }
-
-    componentDidMount() {
-        axios.get(`https://quotes15.p.rapidapi.com/quotes/random/`)
-            .then(res => {
-                const quote = res.data;
-                this.setState({ quote });
-            })
-    }
+class Quote extends React.Component{
+   
     render() {
+        
+        const { quote, name } = this.props;
+
+        const QuoteDetails = (
+            <div>
+                <h1>{quote}</h1>
+                <h2>{name}</h2>
+            </div>
+        );
+
         return(
-            <ul>
-            { this.state.quote.map(quote => <li>{quote.content}</li>) }
-            { this.state.quote.map(quote => <li>{quote.orginator.name}</li>) }
-            </ul>
-        )
+            <Card>
+                {QuoteDetails}
+            </Card>
+        );
     }
 }
+
+Quote.propTypes = {
+    quote: PropTypes.string,
+    name: PropTypes.string
+};
+
+export default Quote;
