b948355587bc6329060b17a7932b98db21e1e316
Samuel Thompson <thomps9012@gmail.com>
2020-09-28 07:18:27 -0400

    api call working correctly, working on importing it into app component

--- src/App.js
+++ src/App.js
@@ -3,22 +3,32 @@ import logo from './vibes.svg';
 import './App.css';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Quote from './quotes/quote';
-import api from './quotes/api';
+import  Card from 'react-bootstrap/Card';
+import axios from '../src/util/api';
+
+
 
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+ constructor(props) {
+   super(props);
+   this.state = {
+     quote: '',
+     author: ''
+   };
+ }
 
-    this.state = {
-      quote: null,
-      name: null
-    };
-  }
+ updateQuote = () => {
+  const quote = axios;
+  console.log(quote);
+
+   this.setState({
+    //  quote: results.data.content,
+    //  author: results.data.originator.name,
+   })
+ }
+
   render(){
-    
-    const { quote, name } = this.state;
 
     return(
 
@@ -32,38 +42,19 @@ class App extends React.Component {
           Put some random positiveness in your day
         </p>
 
-        <Quote quote={quote} name={name} />
-
-        <Button size="lg" variant="primary" onClick={Quote}>
+        <Button size="lg" variant="primary" onClick={this.updateQuote}>
           Yes Please
           </Button>
+          <Card>
+            <h1>{this.state.quote}</h1>
+            <h2>{this.state.author}</h2>
+          </Card>
 
       </header>
     </div>
     );
   }
 
-  async componentDidMount(){
-    
-    let quoteData = await api.get('/', {
-      params: {
-        results: 1,
-        inc: 'quote, name'
-      }
-    });
-
-    quoteData = quoteData.data.results[0];
-
-    const quote = quoteData.content;
-    const name = quoteData.orginator.name;
-
-    this.setState({
-      ...this.state, ...{
-        quote,
-        name
-      }
-    });
-  }
 }
 
 export default App;
